55e14c08cd8940fdc30e4ba112039b269a5fe1d6
MisterChubb <2202561@spsd.sk.ca>
2026-02-12 12:17:11 -0600

    Update sketch.js
    Work for 2/12/2026!

--- Interactive Scene Project/sketch.js
+++ Interactive Scene Project/sketch.js
@@ -10,12 +10,30 @@ function setup() {
 }
 
 function aqauriumBG1(){
-  background('#51719e');
+  background("#51719e");
+  noStroke();
+
+  fill("#1d4d10"); // Dark Seaweed
+  triangle(500, 400, 530, 700, 800, 900);
+
+  fill("#31731f"); // Medium Seaweed
+  triangle(700, 600, 730, 500, 720, 900);
+
+  fill("#6bc253"); // Light Seaweed
+  triangle(730, 650, 780, 600, 720, 930);
+
+  fill("#2F4F4F"); // Dark Rocks
   circle(90, 870, 200);
   circle(200, 880, 100);
-  fill("DarkSlateGrey");
   circle(900, 870, 400);
-  noStroke();
+  circle(700, 870, 200);
+  circle(600, 880, 75);
+
+  fill("#638a8a"); // Light Rocks
+  circle(50, 900, 170);
+  circle(150, 900, 90);
+  circle(200, 900, 40);
+  
 }
 
 
