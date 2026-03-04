379c8e19c6baa430053f278aea3fc5b8a74c8504
MisterChubb <2202561@spsd.sk.ca>
2026-03-04 12:17:11 -0600

    Finished fish scene and started on perlin noise project! :D

--- Miki Hoang - Day in the Life of a Fish (Interactive Scene)/sketch.js
+++ Miki Hoang - Day in the Life of a Fish (Interactive Scene)/sketch.js
@@ -1,17 +1,25 @@
-// Interactive Scene Project (Rename Later)
+// Day in the Life of a Fish (Interactive Scene)
 // Miki Hoang
 // February 11, 2026
 
-// (Short Description Goes Here)
+// It's time to live out your new life as a fish! This is an interactive program where a player can use their mouse cursor to swim around a vast ocean. The "F" key will change the appearance of
+// the fish, while the middle mouse button will change the appearance of the ocean.
 
 
+
+// ----------- GLOBAL VARIABLES ------------
 let fishType = fishDiamond
 let currentBack = 0
 let bgColour = "#51719e"
+// -----------------------------------------
+
+
 
 function setup() {
   createCanvas(900, 900);
 }
+
+
 
 function artistMark(){
   text("Miki Hoang", 750, 880);
@@ -19,33 +27,29 @@ function artistMark(){
   textFont("Serif");
 }
 
+
+
 function oceanBG(){
   background(bgColour);
   noStroke();
+
 
   fill("#876a31"); // Sand
   rect(0, 870, 900, 500);
 
 
-
   fill("#1d4d10"); // Dark Seaweed (Right)
   triangle(600, 400, 630, 700, 750, 900);
-
   fill("#31731f"); // Medium Seaweed (Right)
   triangle(700, 600, 730, 500, 720, 900);
-
   fill("#6bc253"); // Light Seaweed (Right)
   triangle(730, 650, 780, 600, 720, 930);
-
   fill("#1d4d10"); // Dark Seaweed (Left)
   triangle(195, 700, 200, 750, 180, 900);
-
   fill("#31731f"); // Medium Seaweed (Left)
   triangle(200, 780, 230, 750, 170, 900);
-
   fill("#6bc253"); // Light Seaweed (Left)
   triangle(150, 650, 180, 750, 190, 930);
-
 
 
   fill("#2F4F4F"); // Dark Rocks
@@ -80,60 +84,65 @@ function oceanBG(){
 
 }
 
-function fishSquare(){
-  fill("#6afa02");
+
+
+function fishSquare(){ // Draws a pixel fish!
+  fill("#6afa02"); // Tail
   square(mouseX + 140, mouseY + 30, 30);
   square(mouseX + 170, mouseY + 10, 30);
   square(mouseX + 170, mouseY + 50, 30);
 
-  fill("#fa0202");
+  fill("#fa0202"); // Body and Antenna
   rect(mouseX, mouseY, 150, 100);
   rect(mouseX + 20, mouseY - 50, 10, 100);
   rect(mouseX - 30, mouseY - 50, 50, 10);
   rect(mouseX - 30, mouseY - 50, 10, 30);
 
-  fill("#6afa02");
+  fill("#6afa02"); // Face and Fin
   rect(mouseX, mouseY, 60, 100);
   rect(mouseX + 70, mouseY + 40, 30, 20);
 
-   fill("#e3fad2");
+   fill("#e3fad2"); // Eye White and Bulb
   square(mouseX + 10, mouseY + 30, 30);
   square(mouseX - 35, mouseY - 20, 20);
 
-  fill("#0d1c01");
+  fill("#0d1c01"); // Pupil
   square(mouseX + 15, mouseY + 35, 20);
 }
 
-function fishCircle(){
-  fill("#0a21f5");
+
+
+function fishCircle(){ // Draws a round fish!
+  fill("#0a21f5"); // Tail and Top Fin
   circle(mouseX + 120, mouseY - 30, 70);
   circle(mouseX + 120, mouseY + 20, 70);
   circle(mouseX, mouseY - 60, 100);
 
-  fill("#0af5dd");
+  fill("#0af5dd"); // Body
   ellipse(mouseX, mouseY, 200, 150);
 
-  fill("#0a21f5");
+  fill("#0a21f5"); // Face
   circle(mouseX - 30, mouseY, 145);
 
-  fill("#f0f1fc");
+  fill("#f0f1fc"); // Eye White
   circle(mouseX - 50, mouseY, 50);
 
-  fill("#01031a");
+  fill("#01031a"); // Pupil
   circle(mouseX - 50, mouseY, 35);
 }
 
 
-function fishDiamond(){
+
+function fishDiamond(){ // Draws a geometric fish!
   fill("#dbfc05"); // Tail
   triangle(mouseX + 120, mouseY, mouseX + 160, mouseY + 30, mouseX + 160, mouseY - 30);
 
   fill("#eb09d4"); // Body
   quad(mouseX + 140, mouseY, mouseX + 50, mouseY - 50, mouseX, mouseY, mouseX + 50, mouseY + 20);
 
-  fill("#dbfc05");
-  triangle(mouseX + 55, mouseY - 15, mouseX + 85, mouseY - 5, mouseX + 70, mouseY - 20); // Fin
-  triangle(mouseX + 50, mouseY + 20, mouseX + 50, mouseY - 50, mouseX, mouseY); // Face
+  fill("#dbfc05"); // Fin and Face
+  triangle(mouseX + 55, mouseY - 15, mouseX + 85, mouseY - 5, mouseX + 70, mouseY - 20);
+  triangle(mouseX + 50, mouseY + 20, mouseX + 50, mouseY - 50, mouseX, mouseY);
 
   fill("#faf8f7"); // Eye White
   circle(mouseX + 35, mouseY - 10, 20);
@@ -142,7 +151,9 @@ function fishDiamond(){
   circle(mouseX + 35, mouseY - 10, 13);
 }
 
-function keyPressed(){
+
+
+function keyPressed(){ // Changes fish type
   if(keyIsPressed){
     if(key === "f" && fishType === fishDiamond){
       fishType = fishCircle
@@ -156,36 +167,35 @@ function keyPressed(){
   }
 }
 
-function middleMouse(){
+
+
+function mousePressed(){ // Changes background colour
   if(mouseIsPressed){
-    if(mouseButton === CENTER){
-     if(currentBack > 3){
-      currentBack = 0;
-      bgColour = "Red";
-    }
-    else if(currentBack === 0){
-      bgColour = "Red";
-    }
-    else if(currentBack === 1){
-      bgColour = "Yellow";
-    }
-    else if(currentBack === 2){
-      bgColour = "Orange";
-    }
-    else if(currentBack === 3){
-      bgColour = "Blue";
-    }
+   if(mouseButton === CENTER){
     currentBack++;
+    if(currentBack > 3){
+      currentBack = 0;
+    }
+  }
+    if (currentBack === 0){
+      bgColour = "#51719e";
+    }
+    else if (currentBack === 1){
+      bgColour = "#8d519e";
+    }
+    else if (currentBack === 2){
+      bgColour = "#447868";
+    }
+    else if (currentBack === 3){
+      bgColour = "#963636";
+    }
   }
 }
-}
+
+
 
 function draw() {
   oceanBG();
   artistMark();
-  // fishDiamond();
-  // fishCircle();
-  // fishSquare();
   fishType();
-  middleMouse();
  }
