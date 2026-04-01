e3405da2e879b71b9e0076ab77dc0e4ca9716da9
MisterChubb <2202561@spsd.sk.ca>
2026-04-01 12:17:35 -0600

    FINALLY START CARS CARS CARS AAAHHH

---
+++ Cars Cars Cars Project/sketch.js
@@ -0,0 +1,13 @@
+// Cars Cars Cars!
+// Miki Hoang
+// April 1, 2026
+// (INSERT EPIC DESCRIPTION HERE)
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
