ca4ad36c989c08d26990a5ab1acf27f5166b97d3
MisterChubb <2202561@spsd.sk.ca>
2026-04-02 10:59:01 -0600

    A little more work on Cars Cars Cars! :D

--- Cars Cars Cars Project/sketch.js
+++ Cars Cars Cars Project/sketch.js
@@ -8,6 +8,22 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+
+function drawRoad(){
+  fill("#1d1d2b");
+  rect(0, windowHeight/4, windowWidth, windowHeight/2);
+  for (let i = 0; i <= windowWidth; i++){
+    
+  }
+}
+
+
+
+
+
+
+
 function draw() {
-  background(220);
+  background("#1c2616");
+  drawRoad();
 }
