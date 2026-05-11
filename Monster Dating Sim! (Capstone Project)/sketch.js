2e01f70198756f039879e70a117dc3415feb5e1f
MisterChubb <2202561@spsd.sk.ca>
2026-05-11 12:18:01 -0600

    Started Capstone!

---
+++ Monster Dating Sim! (Capstone Project)/sketch.js
@@ -0,0 +1,17 @@
+// Monster Dating Sim! (REPLACE WITH EPIC NAME LATER)
+// Miki Hoang
+// May 11, 2026
+//(EPIC DESCRIPTION GOES HERE)
+
+
+function setup(){
+  createCanvas(1500, 1500);
+}
+
+function startButton(){
+  
+}
+
+function draw(){
+  background(220);
+}
