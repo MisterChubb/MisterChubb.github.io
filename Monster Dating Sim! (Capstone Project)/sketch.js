ba15ae3a6de995eec1a2d5da3bb3ac20dcc7d0a4
MisterChubb <2202561@spsd.sk.ca>
2026-05-12 12:17:17 -0600

    More work on Capstone!

--- Monster Dating Sim! (Capstone Project)/sketch.js
+++ Monster Dating Sim! (Capstone Project)/sketch.js
@@ -3,15 +3,36 @@
 // May 11, 2026
 //(EPIC DESCRIPTION GOES HERE)
 
+let startB;
+let startBImage;
 
+function preload(){
+  startBImage = loadImage("Images/StartB.jpg");
+}
 function setup(){
   createCanvas(1500, 1500);
+  startB = new Clickable();
+  startB.image = startBImage;
+  startB.locate(450, 650);
+  startB.resize(500,200);
+  startB.text = "";
+  // startB.position(450, 750);
+  // startB.size(500, 100);
+  // startB.style("background-image", )
+  // startB.mousePressed(loveInterestSurvey);
 }
 
-function startButton(){
+function gameMenu(){
   
+}
+
+function loveInterestSurvey(){
+  print("Hello!");
+  // startB.hide();
 }
 
 function draw(){
   background(220);
+  startB.draw();
+  // gameMenu();
 }
