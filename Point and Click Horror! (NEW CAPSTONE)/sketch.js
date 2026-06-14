43b53f6dbb817050854a5f1d0c8122784899da2a
MisterChubb <2202561@spsd.sk.ca>
2026-06-13 18:19:49 -0600

    More work on Capstone! Added a function with details for the next scene. Made a new class for cockroach objects. Trying to figure out how to switch between scenes and sprites on mouse click. Also added custom cursor.

--- Point and Click Horror! (NEW CAPSTONE)/sketch.js
+++ Point and Click Horror! (NEW CAPSTONE)/sketch.js
@@ -4,17 +4,40 @@
 // (EPIC DESCRIPTON GOES HERE)
 
 // ----------------------- GLOBAL VARIABLES --------------------------
+let handCursor;
+
+let painting;
+let painting2;
+let paintingState = 0;
+let roach;
+let roach2;
+// let roachState = 0;
+let spawnRoaches = [];
+
 let mirror = [];
 let mirrorIndex = 0;
 let mirrorText = [];
 let pixelFont;
 // -------------------------------------------------------------------
 async function setup() {
-  background(0);
   createCanvas(1000, 1000);
-  pixelFont = await loadFont("Assets/Fonts/pixel2.ttf");
+  noCursor();
+  pixelFont = await loadFont("Assets/Fonts/pixel2.ttf"); // Load new font
+  handCursor = await loadImage("Assets/cursor.PNG");
 
-  for(let i = 1; i < 5; i ++){
+
+
+  painting = await loadImage("Assets/Roach_Puzzle/painting.PNG");
+  painting2 = await loadImage("Assets/Roach_Puzzle/painting2.PNG");
+  roach = await loadImage("Assets/Roach_Puzzle/roach.PNG");
+  roach2 = await loadImage("Assets/Roach_Puzzle/roach2.PNG");
+
+  for(let i = 1; i < 5; i ++){ 
+    spawnRoaches.push(new Roaches());
+  }
+
+
+  for(let i = 1; i < 5; i ++){ // Load mirror scene animation images
     mirror.push(await loadImage("Assets/Mirror_Scene/Mirror" + i + ".PNG"));
   }
 
@@ -31,6 +54,28 @@ function mirrorScene(){
     if(mirrorIndex > 3){
       mirrorIndex = 0;
     }
+  }
+
+  for(let t of mirrorText){
+    t.display();
+    t.move();
+  }
+}
+
+function roachPuzzle(){
+  if(paintingState === 0){
+    image(painting, 0, 0);
+  }
+  if(mouseIsPressed){
+    paintingState = 1;
+  }
+  if(paintingState === 1){
+    image(painting2, 0, 0);
+  }
+
+  for(let r of spawnRoaches){
+    r.display();
+    r.move();
   }
 }
 
@@ -61,20 +106,46 @@ class FloatText{
   }
 }
 
+class Roaches{
+  constructor(){
+    this.x = random(1000);
+    this.y = random(1000);
+    this.sizeW = 100;
+    this.sizeH = 100;
+    this.speedX = random(-3, 3); 
+    this.speedY = random(-3, 3);
+    this.state = 0;
+  }
+  display(){
+    if(paintingState === 1){
+      if(this.state === 0){
+        image(roach, this.x, this.y, this.sizeW, this.sizeH);
+      }
+      if(mouseIsPressed && mouseX === this.x && mouseY === this.y){
+        this.switchState();
+      }
+      if(this.state === 1){
+        image(roach2, this.x, this.y, this.sizeW, this.sizeH);
+      }
+    }
+  }
+  move(){
+    this.x += this.speedX;
+    this.y += this.speedY;
+
+    if (this.x > width) this.x = 0;
+    if (this.x < 0) this.x = width;
+    if (this.y > height) this.y = 0;
+    if (this.y < 0) this.y = height;
+  }
+  switchState(){
+      this.state = 1;
+  }
+}
 
 function draw() {
-  mirrorScene();
-  
-  for(let t of mirrorText){
-    t.display();
-    t.move();
-  }
-
-  if(mouseIsPressed){
-    push();
-    stroke(255);
-    strokeWeight(5);
-    line(mouseX, mouseY, pmouseX, pmouseY);
-    pop();
- }
+  background(0);
+  // mirrorScene();
+  roachPuzzle();
+  image(handCursor, mouseX, mouseY, 100, 100);
 }
